10684cafd4f5746cdc27826609abab2a375d392f
Dev Kumar Jadaun <devthakur6920@gmail.com>
2026-01-17 13:46:51 +0530

    Add intro program with personal detail

--- intro.c
+++ intro.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
     printf("Name: Dev Jadaun");
-    printf("Course: B.Tech CSE");
+    printf("Course: B.Tech (CSE)");
     printf("Language: C Programming");
     return 0;
+
 }
