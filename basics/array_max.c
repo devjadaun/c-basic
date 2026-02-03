01a5527fd017bfaff09222a604422f6b19b714ff
devthakur6920-alt <devthakur6920@gmail.com>
2026-02-03 21:10:31 +0530

    Add 1D array input output and operations

---
+++ basics/array_max.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[5], i, max;
+
+    printf("Enter 5 elements:\n");
+
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+
+    // Finding maximum
+    for (i = 1; i < 5; i++) {
+
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Largest element = %d", max);
+
+    return 0;
+}
